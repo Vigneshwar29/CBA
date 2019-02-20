2b5715429ee585f2f7d31e1655bddfcb806b278a
Vigneshwar29 <36844614+Vigneshwar29@users.noreply.github.com>
2019-02-20 12:51:40 +0530

    Update 213379.c

--- 213379.c
+++ 213379.c
@@ -1,36 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main()
+void main()
 {
-    int a[100],n,i,j,r=0,f=0,l=0;
-    printf("Enter the no. of samples:");
-    scanf("%d",&n);
-    printf("Enter the samples:");
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n-1;i++)
-    {
-        if(a[i]>a[i+1])
-        {
-            printf("Sample Number:%d||Sample Value:%d||Falling Edge\n",i+1,a[i]);
-            f++;
-        }
-        else if(a[i]<a[i+1])
-        {
-            printf("Sample Number:%d||Sample Value:%d||Raising Edge\n",i+1,a[i]);
-            r++;
-        }
-        else if(a[i]==a[i+1])
-        {
-            printf("Sample Number:%d||Sample Value:%d||Level\n",i+1,a[i]);
-            l++;
-        }
-    }
-    printf("No. of Raising edge=%d\n",r);
-    printf("No. of Falling edge=%d\n",f);
-    printf("No. of Level=%d",l);
-    return 0;
+    int a=10,b=10,c;
+    c=a*b;
+    printf("%d",c);
 }
